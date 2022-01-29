47f62ba62176b6e36e65b4637c8045a2dfb4a452
H-137 <matthewleo37@gmail.com>
2022-01-29 12:02:23 -0500

    Added Start and Instructions Menu

---
+++ Assets/Scripts/StartScript.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class StartScript : MonoBehaviour
+{
+    public void loadGame(){
+        SceneManager.LoadScene("GamePlay");
+    }
+
+    public void loadStart(){
+        SceneManager.LoadScene("Start");
+    }
+
+    public void loadInstructions(){
+        SceneManager.LoadScene("Instructions");
+    }
+}
