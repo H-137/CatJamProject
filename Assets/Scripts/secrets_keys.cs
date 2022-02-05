0e789ec8e7f42e54d02e8d50df0604b976a10534
H-137 <matthewleo37@gmail.com>
2022-02-05 15:00:54 -0500

    fixed bugs

--- Assets/Scripts/secrets_keys.cs
+++
@@ -1,7 +0,0 @@
-// Rename this file to `secret_keys.cs` and edit the following lines (containing your actual keys) in order to set secrets
-
-using System.Text;
-
-public class SecretKeys {
-	public static byte[] leaderboard_hmac_key = Encoding.ASCII.GetBytes("bsZ3cODcPbj8guZ8zeDPoOHMqhSyqEO6p2YJ9CT1g6dcUHM0UVcrZdBb7vLUcK1O");
-}
